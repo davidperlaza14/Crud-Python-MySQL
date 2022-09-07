e8105a95cb9de98c0792a0e203c965f3431230f8
davidperlaza14 <davidperlaza14@gmail.com>
2022-09-06 19:35:13 -0500

    CRUD

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from tkinter import *
-
-
-class Ventana(Frame):
-    
-    def __init__(self, master = None):
-        super().__init__(master, width=680, height=260)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-    
-    def create_widgets(self):
-        pass
+from ventana import *
 
 
 def main():
